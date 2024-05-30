cf2d713843f2c8305312a471e870ef927e9f73a8
shray_gla_git <shray.gupta_cs21@gla.ac.in>
2024-05-30 13:29:36 +0530

    idx.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const connectDatabase = require("./database/db.js");
 const corsOptions = {
     // origin: "http://localhost:5175",
     // origin: "https://artgallerycomplete.onrender.com",
-    origin: "https://glittery-gumdrop-b48793.netlify.app/",
+    origin: "https://reliable-quokka-25c833.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     credentials: true,
 };
